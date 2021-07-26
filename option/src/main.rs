918e0442d7aeb65ec7dc3cc9b1e27d427e3edbcd
hxuchen <apehole@163.com>
2021-07-27 01:44:39 +0800

    if let test

--- option/src/main.rs
+++ option/src/main.rs
@@ -43,4 +43,10 @@ fn main() {
     let none = plus_one(None);
     println!("{:?}",n_1);
     println!("{:?}",none);
+    let some_u8_value = Some(1u8);
+    if let Some(3) = some_u8_value {
+        println!("three")
+    }else {
+        println!("unknown")
+    }
 }
