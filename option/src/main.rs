ccadb2880a26929daa05804fc81f1d688a3ee5ea
hxuchen <apehole@163.com>
2021-07-27 00:24:09 +0800

    option<T> test

--- option/src/main.rs
+++ option/src/main.rs
@@ -15,20 +15,32 @@ enum Coin {
     Quarter(UsState),
 }
 
-fn value_in_cents(coin: Coin) -> u32 {
+fn value_in_cents(coin: Coin) -> i32 {
     match coin {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter(state) => {
-            println!("State quarter from {:?}!",state);
+            println!("State quarter from {:?}!", state);
             25
-        },
+        }
     }
 }
 
 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
 fn main() {
     let i = value_in_cents(Coin::Quarter(UsState::Alabama));
-    print!("{}",i)
+    println!("{}", i);
+    let n = Some(i);
+    let n_1 = plus_one(n);
+    let none = plus_one(None);
+    println!("{:?}",n_1);
+    println!("{:?}",none);
 }
