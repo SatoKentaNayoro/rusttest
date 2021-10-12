7ee34cc2d86f9a02fda0b4b63ceafdabed4468fc
hxuchen <apehole@163.com>
2021-10-12 23:13:53 +0800

    web server test

---
+++ web_server_test/target/debug/deps/web_server_test.d
@@ -0,0 +1,5 @@
+/Users/huangxuchen/RustProj/rusttest/web_server_test/target/debug/deps/web_server_test: src/main.rs
+
+/Users/huangxuchen/RustProj/rusttest/web_server_test/target/debug/deps/web_server_test.d: src/main.rs
+
+src/main.rs:
---
+++ web_server_test/target/debug/web_server_test.d
@@ -0,0 +1,1 @@
+/Users/huangxuchen/RustProj/rusttest/web_server_test/target/debug/web_server_test: /Users/huangxuchen/RustProj/rusttest/web_server_test/src/main.rs
