dd140130b02aa958badb3c65b3f1202ab5e1d6b0
hxuchen <apehole@163.com>
2021-10-13 22:10:13 +0800

    web server test2

--- web_server_test/src/main.rs
+++ web_server_test/src/main.rs
@@ -1,7 +1,6 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
-use std::fs::File;
-use std::ptr::null;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -17,18 +16,16 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
     let get = b"GET / HTTP/1.1\r\n";
-    let mut contents = String::new();
-    let mut response = String::new();
-    if buffer.starts_with(get) {
-        let mut file = File::open("hello.html").unwrap();
-        file.read_to_string(&mut contents).unwrap();
-        // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    }
-    if contents.is_empty() {
-        response = String::from("HTTP/1.1 404 NOT FOUND\r\n\r\n");
-    } else {
-        response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-    }
+
+    let (status_line,filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n","hello.html")
+    }else {
+        ("HTTP/1.1 404 NOT FOUND\r\n","404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}Content-Length: {}\r\n\r\n{}",status_line,contents.len(),contents);
+
     stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap()
+    stream.flush().unwrap();
 }
